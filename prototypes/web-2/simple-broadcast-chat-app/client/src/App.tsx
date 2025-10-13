8608bdc502e17aae504a6d691c85e127b5b163b0
Sabarish-developer <sabarish.cspro@gmail.com>
2025-10-13 12:22:12 +0530

    prototype: add simple-broadcast-chat-app server

---
+++ prototypes/web-2/simple-broadcast-chat-app/client/src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-amber-400">heelo</div>
+    </>
+  )
+}
+
+export default App
---
+++ prototypes/web-2/simple-broadcast-chat-app/server/src/index.ts
@@ -0,0 +1,62 @@
+import {WebSocketServer, WebSocket} from 'ws';
+
+interface MyWebSocket extends WebSocket{
+	clientName?: string
+}
+
+const wss = new WebSocketServer({port: 8080});
+
+let userCount = 0;
+const map = new Map<number, MyWebSocket[]>(); // 1: [socket1, socket2]
+
+wss.on("connection", (socket) => {
+	userCount++;
+	console.log("Total users: ", userCount);
+
+	socket.on("message", (message) => {
+		
+		const parsedMessage = JSON.parse(message.toString()); // parsing the message, string -> json
+
+		// socket client joining a room
+		if(parsedMessage.type === 'join'){
+			const roomId = parsedMessage.payload.roomId;
+			const sockets = map.get(roomId) ?? [];
+			const mySocket = socket as MyWebSocket;
+			mySocket.clientName = parsedMessage.payload.clientName;
+			sockets.push(mySocket);
+			map.set(roomId, sockets);
+			console.log('Client: ', mySocket.clientName, 'joined Room: ', roomId);
+		}
+
+		// socket client broadcasting a message to all clients in same room
+		if(parsedMessage.type === 'broadcast'){
+			for(const [roomId, sockets] of map.entries()){
+				if(sockets.includes(socket)){
+					const mySocket = socket as MyWebSocket;
+					const senderName = mySocket.clientName;
+					sockets.forEach((s) => {
+						if(s !== socket){
+							s.send(senderName+": "+parsedMessage.payload.message);
+						}
+					
+					})
+					break;
+				}
+			}
+		}
+	})
+
+	socket.on("close", () => {
+		for(const [roomId, sockets] of map.entries()){
+			const index = sockets.indexOf(socket);
+			if(index !== -1){
+				sockets.splice(index, 1);
+				map.set(roomId, sockets);
+				break;
+			}
+		}
+	})
+
+})
+
+
