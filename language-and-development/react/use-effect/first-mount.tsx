1dab957cf543727ee575ce6ae55d5cc352ed2193
Sabarish-developer <sabarish.cspro@gmail.com>
2025-12-14 19:24:27 +0530

    concept: react lifecycle

--- language-and-development/react/use-effect/first-mount.tsx
+++ language-and-development/react/use-effect/first-mount.tsx
@@ -13,6 +13,16 @@ export const FirstMount = () => {
     </div>
 }
 
-// first component code and return is executed, then it is commited -> virtual dom -> real dom
+// first component code and return is executed
 // then use effect runs
 // useEffect runs after 10 ms for this code
+
+// 2 phases: Render and Commit
+// Render phase: calculate ui, update state,props etc (Pure code), -> then creates a virtual dom tree
+//               if re-render, creates a new virtual dom tree then compare old vs new for changes - reconciliation
+
+// Commit phase: first cleanup previous effects
+//               apply updates to real dom
+//               run useLayoutEffect (synchronously)
+//               Browser paints the ui to screen
+//               run effects - useEffect (asynchronously)
