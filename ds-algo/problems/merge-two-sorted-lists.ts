115f4e24e0ee44aed563df3894ba33406fd6b7e1
Sabarish-codes <sabarish.cspro@gmail.com>
2026-09-22 00:28:25 +0530

    problem: merge two sorted lists (simple if-else logic)

---
+++ ds-algo/problems/merge-two-sorted-lists.ts
@@ -0,0 +1,38 @@
+
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+    
+    if(!list1 && !list2) return null;
+    if(!list1) return list2;
+    if(!list2) return list1;
+
+    const head = new ListNode();
+    let current = head;
+    while(list1 && list2){
+        if(list1.val <= list2.val){
+            current.next = list1;
+            list1 = list1.next;
+        }
+        else{
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next;
+    }
+    current.next = list1 || list2;
+
+    return head.next;
+};
+
+/*
+Time - O(n+m)
+Space - O(1)
+*/
